a2b415c0345272ce7f69348ab7fa975d74ef0ef5
Kurt <nimsolo@gmail.com>
2020-01-21 12:53:52 +0100

    first commit

---
+++ cif/CipherService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace cif
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class CipherService
+	{
+		private const int KeySize = 128;
+		private const int DerivationIterations = 1000;
+
+		public static string Encrypt(string plainText, string passPhrase)
+		{
+			var saltStringBytes = Generate256BitsOfRandomEntropy();
+			var ivStringBytes = Generate256BitsOfRandomEntropy();
+			var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+			using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+			{
+				var keyBytes = password.GetBytes(KeySize / 8);
+				using (var symmetricKey = new RijndaelManaged())
+				{
+					symmetricKey.BlockSize = 128;
+					symmetricKey.Mode = CipherMode.CBC;
+					symmetricKey.Padding = PaddingMode.PKCS7;
+					using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+					{
+						using (var memoryStream = new MemoryStream())
+						{
+							using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+							{
+								cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+								cryptoStream.FlushFinalBlock();
+								var cipherTextBytes = saltStringBytes;
+								cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+								cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+								memoryStream.Close();
+								cryptoStream.Close();
+								return Convert.ToBase64String(cipherTextBytes);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		private static byte[] Generate256BitsOfRandomEntropy()
+		{
+			var randomBytes = new byte[16];
+			using (var rngCsp = new RNGCryptoServiceProvider())
+			{
+				rngCsp.GetBytes(randomBytes);
+			}
+			return randomBytes;
+		}
+	}
+}
---
+++ cif/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace cif
+{
+	class Program
+	{
+
+		/// <summary>
+		/// cif is a tool to encrypt or decrypt any text by using a secret key.
+		/// </summary>
+		/// <param name="action">The secret required to encrypt or decrypt.</param>
+		/// <param name="args">-secret is the key to encrypt while -text is the text to encrypt.</param>
+		public static async Task Main(string action, string[] args = null)
+		{
+
+			Console.WriteLine("Welcome to Cif the encryptor and decryptor!");
+
+			var encryptCommand = new RootCommand(action)
+			{
+				new Option("-secret") {Argument = new Argument<string>()},
+				new Option("-text") {Argument = new Argument<string>()}
+			};
+
+
+			encryptCommand.Handler = CommandHandler.Create<string, string>(Encrypt);
+
+			await encryptCommand.InvokeAsync(args);
+		}
+
+
+		public static void Encrypt(string secret, string text)
+		{
+			var cipher = CipherService.Encrypt(text, secret);
+
+			Console.WriteLine($"The cipher is: {cipher}");
+		}
+	}
+}
